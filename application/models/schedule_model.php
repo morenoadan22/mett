6bd45d2cd9ad87db0a59d0a589aebf556f9d5f30
Adan Moreno <morenoadan22@gmail.com>
2014-10-08 14:56:25 -0700

    Initial commit

---
+++ application/models/schedule_model.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * ScheduleModel
+ * This is basically a simple CRUD (Create/Read/Update/Delete) demonstration.
+ */
+class ScheduleModel
+{
+    /**
+     * Constructor, expects a Database connection
+     * @param Database $db The Database object
+     */
+    public function __construct(Database $db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * Getter for all exams (exams are an implementation of example data, in a real world application this
+     * would be data that the user has created)
+     * @return array an array with several objects (the results)
+     */
+    public function getAllSchedules()
+    {
+        $sql = "SELECT user_id, exam_id, exam_text FROM exams WHERE user_id = :user_id";
+        $query = $this->db->prepare($sql);
+        $query->execute(array(':user_id' => $_SESSION['user_id']));
+
+        // fetchAll() is the PDO method that gets all result rows
+        return $query->fetchAll();
+    }
+
+    /**
+     * Getter for a single exam
+     * @param int $exam_id id of the specific exam
+     * @return object a single object (the result)
+     */
+    public function getSchedule($exam_id)
+    {
+        $sql = "SELECT user_id, exam_id, exam_text FROM exams WHERE user_id = :user_id AND exam_id = :exam_id";
+        $query = $this->db->prepare($sql);
+        $query->execute(array(':user_id' => $_SESSION['user_id'], ':exam_id' => $exam_id));
+
+        // fetch() is the PDO method that gets a single result
+        return $query->fetch();
+    }
+
+    /**
+     * Setter for a exam (create)
+     * @param string $exam_text exam text that will be created
+     * @return bool feedback (was the exam created properly ?)
+     */
+    public function create($exam_text)
+    {
+        // clean the input to prevent for example javascript within the exams.
+        $exam_text = strip_tags($exam_text);
+
+        $sql = "INSERT INTO exams (exam_text, user_id) VALUES (:exam_text, :user_id)";
+        $query = $this->db->prepare($sql);
+        $query->execute(array(':exam_text' => $exam_text, ':user_id' => $_SESSION['user_id']));
+
+        $count =  $query->rowCount();
+        if ($count == 1) {
+            return true;
+        } else {
+            $_SESSION["feedback_negative"][] = FEEDBACK_NOTE_CREATION_FAILED;
+        }
+        // default return
+        return false;
+    }
+
+    /**
+     * Setter for a exam (update)
+     * @param int $exam_id id of the specific exam
+     * @param string $exam_text new text of the specific exam
+     * @return bool feedback (was the update successful ?)
+     */
+    public function editSave($exam_id, $exam_text)
+    {
+        // clean the input to prevent for example javascript within the exams.
+        $exam_text = strip_tags($exam_text);
+
+        $sql = "UPDATE exams SET exam_text = :exam_text WHERE exam_id = :exam_id AND user_id = :user_id";
+        $query = $this->db->prepare($sql);
+        $query->execute(array(':exam_id' => $exam_id, ':exam_text' => $exam_text, ':user_id' => $_SESSION['user_id']));
+
+        $count =  $query->rowCount();
+        if ($count == 1) {
+            return true;
+        } else {
+            $_SESSION["feedback_negative"][] = FEEDBACK_NOTE_EDITING_FAILED;
+        }
+        // default return
+        return false;
+    }
+
+    /**
+     * Deletes a specific exam
+     * @param int $exam_id id of the exam
+     * @return bool feedback (was the exam deleted properly ?)
+     */
+    public function delete($exam_id)
+    {
+        $sql = "DELETE FROM exams WHERE exam_id = :exam_id AND user_id = :user_id";
+        $query = $this->db->prepare($sql);
+        $query->execute(array(':exam_id' => $exam_id, ':user_id' => $_SESSION['user_id']));
+
+        $count =  $query->rowCount();
+
+        if ($count == 1) {
+            return true;
+        } else {
+            $_SESSION["feedback_negative"][] = FEEDBACK_NOTE_DELETION_FAILED;
+        }
+        // default return
+        return false;
+    }
+}
---
+++ application/views/index/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<div class="content">
+
+  <h1 align="center">Computer Science Master&#8217;s Exams</h1>
+    <!-- echo out the system feedback (error and success messages) -->
+    <?php $this->renderFeedbackMessages(); ?>
+
+<p>Graduate students selecting Plan B must pass three of five possible written comprehensive exams. Students can select any three exams of Automata-Formal language, Data Structures-Algorithms, Numerical Analysis, Operating Systems-Architecture, and Programming Languages. The syllabi for the material covered on each exam is available on line by selecting the exam above. Copies of previous exams are available at Cal Copy, 5131 College Avenue; call 619-582-9949 for hours.</p>
+
+<p>Students are allowed three attempts to pass each local exam. That is, students can take up to three tries on each of the local exams. However, the (non-local!) GRE Computer Science subject exam has no limit on the number of attempts.  **Note the GRE CS subject exam is being discontinued by ETS after April 2013**</p>
+
+<p>Deadline to register for exams is one week before the exam is offered. No exceptions. Students can start signing up for the fall exams on August 1 and the spring exams on December 5. If a student cannot take an exam they must cancel at least one week before the exam is offered. If the cancellation is not done on time, the test will count as an attempt at the exam scheduled even if the student does not take the exam.</p>
+
+<p>To sign-up or cancel for an exam, please contact the Department Office by email: <a href="mailto:mathhelp@sciences.sdsu.edu">mathhelp@sciences.sdsu.edu</a>. Include your full name, mailing address, phone number, REDID number, email address, and title of exam.</p>
+
+<p>The master&#8217;s exams are offered two times a year before the start of the Fall semester and before the start of the Spring semester. The exams are offered on three consecutive weekends. Each exam is only offer once per semester. Please refer to the <a href="http://www.cs.sdsu.edu/grad/exams/schedule.html">online schedule</a> for dates, times, and exam locations.</p>
+
+<p>When exams have been graded, you will be notified via email. Please do not contact the Department Office inquiring about your grades. Remember the test number assigned to you!</p>
+
+<p>For more information about the Master&#8217;s exams contact a <a href="http://www.cs.sdsu.edu/grad/advisors.html">graduate student advisor</a>.</p>
+
+<p>Upon successfully completing and passing three requisite Master&#8217;s exams, and</p>
+<ul type="disc">
+<li>have taken or had waived the GRE;</li>
+<li>have an Official Program of Study on file; and</li>
+<li>attained Advancement to Candidacy,</li>
+</ul>
+
+<p>Please contact the Department Office via email: <a href="mailto:mathhelp@sciences.sdsu.edu">mathhelp@sciences.sdsu.edu</a>. Include your full name, RedID number, the name of the exams you have passed, and the semester in which you passed the exams. You will be notified via email that you have met the requirements for graduation and a copy of the Report of Final Examinations form will be emailed to you. Print the form, fill in the details, have your academic advisor sign it, and then take the form to the Graduate Division. This form is needed to complete your graduation requirements, without which you will not be processed for graduation.</p>
+</body>
+</html>
