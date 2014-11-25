0ffbeb654a32a72fa8a69a83114184b3952d72c3
morenoadan22 <morenoadan22@gmail.com>
2014-11-25 00:04:01 -0800

    changed date from object to static method

--- application/models/schedule_model.php
+++ application/models/schedule_model.php
@@ -101,7 +101,7 @@ class ScheduleModel
      */
     public function getUpcomingExams()
     {
-    	$curYear = new Date("Y");
+    	$curYear = date("Y");
     	$sql = "SELECT exam_schedule.id, exam_type.type AS exam_type , location, date, time, semester, year,"; 
     	$sql .= "(select count(student_exam.exam_schedule) FROM student_exam WHERE student_exam.exam_schedule = exam_schedule.id)";
     	$sql .= " AS student_count, (select id from student_exam where student_exam.exam_schedule = exam_schedule.id) AS student_exam_id, FROM exam_schedule JOIN exam_type ON exam_schedule.exam_type = exam_type.id WHERE year >= :current_year";
