ed6fdbca0663cd722af3e8b33dc1da91a0cb70c5
morenoadan22 <morenoadan22@gmail.com>
2014-11-25 01:02:05 -0800

    Use javascript to handle register actions.

--- public/js/register.js
+++ public/js/register.js
@@ -7,3 +7,11 @@ $(document).ready(function(){
 	});
 	
 });
+
+function handleEnrollment(examId){
+	window.location.href = "enrollSave/" + examId;
+}
+
+function handleUnregister(studentExamId){
+	window.location.href = "enrollRemove/" + studentExamId;
+}
