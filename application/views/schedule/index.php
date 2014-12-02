2453cd42634de437ada4c5f494b0ec94304363ac
root <root@ip-172-31-14-95.us-west-1.compute.internal>
2014-12-02 01:12:48 +0000

    update view of schedule

--- application/views/schedule/index.php
+++ application/views/schedule/index.php
@@ -1,22 +1,25 @@
 <head>
     <meta charset="UTF-8"/>
 	
-	<link rel="stylesheet" href="../public/css/schedule.css" />
+	<link rel="stylesheet" href="../public/css/bootstrap.css" /> 
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
 	<script type="text/javascript" src="../public/js/schedule.js"></script>
 </head>
 
 <body>
-	
+<div class = "container" >	
+	<div class="row">
+	<div class="col-md-3 col-sd-12">
+
 	<h1>Schedule</h1>
 	
 	 <!-- echo out the system feedback (error and success messages) -->
 	 <?php $this->renderFeedbackMessages(); ?>
 	
-	<div id="scheduleForm">
+	<div id="scheduleForm" class = "form-group">
 		<form method="post" action="<?php echo URL;?>schedule/addSchedule" onSubmit="return validateFormOnSubmit(this)">
 			<label for="selectExamType">Exam Type</label>
-			<select name="selectExamType" required>
+			<select class="form-control" name="selectExamType" required>
 				<option value="">--Select One--</option>
 				<?php 
 					if($this->examTypes){
@@ -29,23 +32,32 @@
 					}
 				?>
 			</select>
-			<label for="textLocation">Room Number</label><input name="textLocation" type="text">
-			<label for="textDate">Date</label><input name="textDate" type="date">
-			<label for="textTime">Time</label><input name="textTime" type="time">
-			<label for="selectSemester">Semester</label><select name="selectSemester">
+		
+				<label for="textLocation">Room Number </label><input class="form-control" name="textLocation" type="text">
+				
+				<label for="textDate">Date</label><input class="form-control" name="textDate" type="date"> 
+		
+				<label for="textTime">Time</label><input class="form-control" name="textTime" type="time">
+		
+				<label for="selectSemester">Semester</label>
+				<select class="form-control" name="selectSemester">
 				<option value="FALL">Fall</option>
 				<option value="SPRING">Spring</option>
 				<option value="SUMMER">Summer</option>
-			</select>	
-			<label for="selectYear">Year</label><select id="years" name="selectYear"></select><br>		
-			<input type="submit" value='Add Exam' autocomplete="off" />
+				</select>	
+		
+			<label for="selectYear">Year</label><select class="form-control" id="years" name="selectYear"></select><br>		
+		
+			<input class="form-control" type="submit" value='Add Exam' autocomplete="off" />
+		
 		</form>
 	</div>	
-	
-	<h1 style="margin-top: 50px;">List of Exam Schedules</h1>
-	
-	<table id="examTable">
-		<tr><th>EXAM TYPE</th><th>LOCATION</th><th>STUDENT COUNT</th><th>DATE</th></tr>
+	</div>	
+	<div class="col-md-9 col-sd-12">
+	<h1>List of Exam Schedules</h1>
+	<div class="table-responsive">	
+	<table id="examTable" class="table">
+		<tr><th>EXAM TYPE</th><th>LOCATION</th><th style="white-space:nowrap;">STUDENT COUNT</th><th>DATE</th><th>TIME</th><th></th><th></th></tr>
 		<?php
 			if ($this->schedules) {
 				foreach($this->schedules as $key => $value) {
@@ -64,8 +76,10 @@
 			}
 		?>
 		</table>
-	
-		
+	</div>
+	</div>
+	</div>
+</div>
 </body>
 	
 	
