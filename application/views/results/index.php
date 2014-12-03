e55fe9383bb240d041de3d6c8ffd0f9f0de6a16e
Adan Moreno <morenoadan22@gmail.com>
2014-12-03 14:49:31 -0800

    Fixed exam result text

--- application/views/results/index.php
+++ application/views/results/index.php
@@ -1,30 +1,13 @@
 <div class="content">
-    <h1>Exam List</h1>
-    <h3>List of the exam results you now enroll in.</h3>
+    <h1>Exam Results</h1>
+    <h3>List of the exam scores.</h3>
 
     <!-- echo out the system feedback (error and success messages) -->
     <?php $this->renderFeedbackMessages(); ?>
 
     <form method="post" action="<?php echo URL;?>note/create">
-        <label>Text of new note: </label><input type="text" name="note_text" />
+        <label>Exam Result: </label><input type="text" name="note_text" />
         <input type="submit" value='Create this note' autocomplete="off" />
     </form>
 
-    <h1 style="margin-top: 50px;">List of your previous exam.</h1>
-
-    <table class = "table">
-    <?php
-        if ($this->notes) {
-            foreach($this->notes as $key => $value) {
-                echo '<tr>';
-                echo '<td>' . htmlentities($value->note_text) . '</td>';
-                echo '<td><a href="'. URL . 'note/edit/' . $value->note_id.'">Edit</a></td>';
-                echo '<td><a href="'. URL . 'note/delete/' . $value->note_id.'">Delete</a></td>';
-                echo '</tr>';
-            }
-        } else {
-            echo 'No exam registered yet. Register now!';
-        }
-    ?>
-    </table>
 </div>
